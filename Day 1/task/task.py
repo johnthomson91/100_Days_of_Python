e5b57fff8affa3da0cfb26d95d7f6e7c0ec4b38b
John Thomson <johnthomson91@gmail.com>
2025-02-05 19:25:49 -0500

    day 1 code

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+print("welcome to the band name generator")
+
+city = input("what city did you grow up in?")
+
+pet = input("what is the name of your pet?")
+print(f"your band name could be {city} {pet}")
