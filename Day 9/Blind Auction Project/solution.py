30eee4cfd25b48ac2d1bbbac1f4de2014b3453a9
John Thomson <johnthomson91@gmail.com>
2025-02-07 19:01:41 -0500

    Day 9/ Blind Auction

---
+++ Day 9/Blind Auction Project/solution.py
@@ -0,0 +1,22 @@
+from art import logo
+print(logo)
+
+
+def find_highest_bidder(bids_dict):
+    highest_bid = max(bids.values())
+    winner = max(bids, key=bids.get)
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)
